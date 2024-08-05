325e5594b382bba7771bdda105b6a094d1f7d886
milton <caizhihao.milton@gmail.com>
2024-08-05 20:35:13 +0800

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,57 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	compatibilityDate: '2024-04-03',
+	devtools: { enabled: true },
+	srcDir: 'src/',
+	dir: {
+		public: 'public',
+	},
+
+	devServer: {
+		host: '0.0.0.0',
+		port: 3090,
+	},
+
+	nitro: {
+		compressPublicAssets: {
+			gzip: true,
+			brotli: true,
+		},
+	},
+
+	app: {
+		head: {
+			charset: 'utf-8',
+			viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, viewport-fit=cover',
+			title: 'Cartbank',
+			link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
+		},
+		pageTransition: { name: 'page', mode: 'out-in' },
+		layoutTransition: { name: 'layout', mode: 'out-in' },
+	},
+
+	plugins: [{ src: '~/plugins/aos.client.ts', mode: 'client', ssr: false }],
+
+	router: {
+		options: {
+			scrollBehaviorType: 'smooth',
+		},
+	},
+
+	css: ['~/assets/styles/common.scss', '~/assets/styles/reset.scss'],
+
+	vite: {
+		css: {
+			preprocessorOptions: {
+				additionalData: '@import "@/assets/styles/default.scss";',
+			},
+		},
+		optimizeDeps: {
+			exclude: ['*.ts', '*.vue'],
+		},
+	},
+
+	build: {
+		analyze: true,
+	},
+})
---
+++ src/plugins/aos.client.ts
@@ -0,0 +1,16 @@
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { defineNuxtPlugin } from '#app'
+
+export default defineNuxtPlugin(nuxtApp => {
+	nuxtApp.vueApp.use({
+		install(Vue) {
+			Vue.directive('aos', {
+				mounted(el, binding) {
+					el.setAttribute('data-aos', binding.value)
+					AOS.init()
+				},
+			})
+		},
+	})
+})
